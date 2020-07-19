2ceb4d71f90fcd5e304700798d32ae13bcd65170
Melvin <mel000vin@yahoo.com>
2020-07-19 14:46:34 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,14 +5,3 @@ terraform {
 provider "vault" {
   version = "~> 2.9"
 }
-
-resource "vault_generic_secret" "example" {
-  path = "secret/foo"
-
-  data_json = <<EOT
-{
-  "foo":   "bar",
-  "pizza": "cheese"
-}
-EOT
-}
