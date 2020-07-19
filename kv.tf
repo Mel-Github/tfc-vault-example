47eb75cb9c3e9a859cca0c43385699dc8464c3d8
Mel-GitHub <mel000vin@yahoo.com>
2020-07-19 17:55:21 +0800

    modified json

--- kv.tf
+++ kv.tf
@@ -3,14 +3,14 @@ resource "vault_mount" "app" {
   type = "generic"
 }
 
-
+git a
 resource "vault_generic_secret" "example" {
   path = "${vault_mount.app.path}/pipeline/docker"
 
   data_json = <<EOT
 {
   "DOCKER_LOGIN":   "${var.docker_username}",
-  "DOCKER_PWD": "${var.docker_password}",
+  "DOCKER_PWD": "${var.docker_password}"
 }
 EOT
 }
