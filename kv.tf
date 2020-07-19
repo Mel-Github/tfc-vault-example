bc0fa488d32540310ac06b5567b89db2d633d264
Mel-GitHub <mel000vin@yahoo.com>
2020-07-19 17:51:50 +0800

    added docker variable

--- kv.tf
+++ kv.tf
@@ -2,3 +2,15 @@ resource "vault_mount" "app" {
   path = var.app
   type = "generic"
 }
+
+
+resource "vault_generic_secret" "example" {
+  path = "${vault_mount.app.path}/pipeline/docker"
+
+  data_json = <<EOT
+{
+  "DOCKER_LOGIN":   "${var.docker_username}",
+  "DOCKER_PWD": "${var.docker_password}",
+}
+EOT
+}
--- variables.tf
+++ variables.tf
@@ -2,3 +2,15 @@ variable "app" {
   type        = string
   description = "name of app"
 }
+
+
+variable "docker_username" {
+  type        = string
+  description = "docker user name"
+}
+
+
+variable "docker_password" {
+  type        = string
+  description = "docker user password"
+}
