3bd81b08ff37454363e1d89a4dbbd5391443de83
Melvin <mel000vin@yahoo.com>
2020-07-19 11:43:41 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,9 @@
+terraform {
+  required_version = "~>0.12"
+}
+
 provider "vault" {
-  # It is strongly recommended to configure this provider through the
-  # environment variables described above, so that each user can have
-  # separate credentials set in the environment.
-  #
-  # This will default to using $VAULT_ADDR
-  # But can be set explicitly
-  # address = "https://vault.example.net:8200"
+  version = "~> 2.9"
 }
 
 resource "vault_generic_secret" "example" {
